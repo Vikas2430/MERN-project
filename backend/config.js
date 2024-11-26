22d8bbc7f3993a5bda51b5e35dc364722cb252e3
Vikas2430 <sharma.vikas@606@gmail.com>
2024-11-26 14:10:38 +0530

    updated

--- backend/config.js
+++ backend/config.js
@@ -3,8 +3,3 @@ dotenv.config();
 
 export const PORT = process.env.PORT || 5555;
 export const mongoDBURL = process.env.MONGO_DB_URL;
-
-
-// export const PORT = process.env.PORT || 5555;
-
-// export const mongoDBURL = "mongodb+srv://root:root@book-store-mern.4xyef.mongodb.net/book-collection?retryWrites=true&w=majority&appName=Book-store-MERN";
