c3f3279dc976a17501bf91b3d98352426272ee3d
Vikas2430 <sharma.vikas@606@gmail.com>
2024-11-27 16:35:46 +0530

    updated

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -45,7 +45,7 @@ const Home = () => {
             </tr>
           </thead>
           <tbody>
-            {Book.map((book, index) => (
+            {book.map((book, index) => (
               <tr key={book._id} className='h-8'>
                 <td className='border border-slate-600 rounded-md text-center'>
                   {index + 1}
