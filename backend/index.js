ad8401b6a088c5606699a1e831f89ad86bb738ab
Vikas2430 <sharma.vikas@606@gmail.com>
2024-11-27 15:39:26 +0530

    updated

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,16 @@ import mongoose from "mongoose";
 import booksRoute from "./routes/booksRoute.js"
 import cors from 'cors';
 
+
+const corsOptions = {
+    origin: "https://mern-project-1zr5.vercel.app", 
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true, 
+  };
+  
 const app = express();
+
+app.use(cors(corsOptions));
 
 app.use(express.json());
 
