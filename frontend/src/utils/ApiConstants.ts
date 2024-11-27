1d7797d1822673e8f50d567676a8142cdc7c4c70
Vikas2430 <sharma.vikas@606@gmail.com>
2024-11-27 14:20:26 +0530

    updated

--- frontend/src/utils/ApiConstants.ts
+++ frontend/src/utils/ApiConstants.ts
@@ -1,4 +1,4 @@
-export const baseUrl = "https://mern-project-opal.vercel.app/"
+export const baseUrl = "https://vercel.com/vikas-projects-cfcedde7/mern-project/5tbgXbGbBoriQVk8mzLt2J5qAga9"
 // export const baseUrl = "http://localhost:5555/book"
 
 export const book = baseUrl+'/book'
