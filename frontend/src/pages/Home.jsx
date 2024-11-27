b0473400f4aca49b4f01aa1c996263e3a4c00a08
Vikas2430 <sharma.vikas@606@gmail.com>
2024-11-27 14:16:34 +0530

    updated

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -5,7 +5,6 @@ import { Link } from 'react-router-dom';
 import { AiOutlineEdit } from 'react-icons/ai';
 import { BsInfoCircle } from 'react-icons/bs';
 import { MdOutlineAddBox, MdOutlineDelete } from 'react-icons/md';
-import { baseUrl } from '../utils/ApiConstants';
 
 const Home = () => {
   const [Book, setBook] = useState([]);
@@ -13,7 +12,7 @@ const Home = () => {
   useEffect(() => {
     setLoading(true);
     axios
-      .get(baseUrl)
+      .get('http://localhost:5555/book')
       .then((response) => {
         setBook(response.data.data);
         setLoading(false);
