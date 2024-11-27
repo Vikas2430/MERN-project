4ac41a39123c5f1c44f2aee7e1f90546616d9596
Vikas2430 <sharma.vikas@606@gmail.com>
2024-11-27 14:52:56 +0530

    updated

--- frontend/src/utils/ApiConstants.ts
+++ frontend/src/utils/ApiConstants.ts
@@ -1,4 +1,4 @@
-export const baseUrl = "https://mern-project-opal.vercel.app/"
+export const baseUrl = "https://mern-project-1zr5.vercel.app/"
 // export const baseUrl = "http://localhost:5555/book"
 
 export const book = baseUrl;
